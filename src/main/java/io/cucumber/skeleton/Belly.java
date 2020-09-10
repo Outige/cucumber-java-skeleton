5ccbe6a5ce105610b8efaf4c11ea10a7aa26fe03
Tieg <tiegosullivanpsnl@gmail.com>
2020-09-10 11:24:35 +0200

    :sparkles: working trivial cucumber test

--- src/main/java/io/cucumber/skeleton/Belly.java
+++ src/main/java/io/cucumber/skeleton/Belly.java
@@ -4,4 +4,12 @@ public class Belly {
     public void eat(int cukes) {
 
     }
+    
+    public void wait(int waitTime) {
+
+    }
+
+    public void growl() {
+
+    }
 }
--- src/test/java/io/cucumber/skeleton/StepDefinitions.java
+++ src/test/java/io/cucumber/skeleton/StepDefinitions.java
@@ -1,11 +1,25 @@
 package io.cucumber.skeleton;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
 
 public class StepDefinitions {
     @Given("I have {int} cukes in my belly")
     public void I_have_cukes_in_my_belly(int cukes) {
         Belly belly = new Belly();
         belly.eat(cukes);
+    }
+
+    @When("I wait {int} hour")
+    public void i_wait_hour(int waitTime) {
+        Belly belly = new Belly();
+        belly.wait(waitTime);
+    }
+
+    @Then("my belly should growl")
+    public void my_belly_should_growl() {
+        Belly belly = new Belly();
+        belly.growl();
     }
 }
